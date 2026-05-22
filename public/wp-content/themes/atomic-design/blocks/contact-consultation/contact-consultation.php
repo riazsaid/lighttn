3ba1149b5c36f828fd8f6210eef90e0effacd229
riazsaid <riazsaid15@gmail.com>
2026-05-22 05:47:48 +0500

    added contact page forms following design

---
+++ public/wp-content/themes/atomic-design/blocks/contact-consultation/contact-consultation.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Contact Consultation Block Template (acf/contact-consultation)
+ */
+
+$heading = function_exists('get_field') ? (get_field('contact_consultation_heading') ?: '') : '';
+$subheading = function_exists('get_field') ? (get_field('contact_consultation_subheading') ?: '') : '';
+$form_heading = function_exists('get_field') ? (get_field('contact_consultation_form_heading') ?: '') : '';
+$form_intro = function_exists('get_field') ? (get_field('contact_consultation_form_intro') ?: '') : '';
+$booking_heading = function_exists('get_field') ? (get_field('contact_consultation_booking_heading') ?: '') : '';
+$booking_subheading = function_exists('get_field') ? (get_field('contact_consultation_booking_subheading') ?: '') : '';
+$booking_points = function_exists('get_field') ? (get_field('contact_consultation_booking_points') ?: '') : '';
+$map_embed_url = function_exists('get_field') ? (get_field('contact_consultation_map_embed_url') ?: '') : '';
+
+get_template_part(
+    'template-parts/shared/contact-consultation',
+    null,
+    [
+        'heading'            => $heading,
+        'subheading'         => $subheading,
+        'form_heading'       => $form_heading,
+        'form_intro'         => $form_intro,
+        'booking_heading'    => $booking_heading,
+        'booking_subheading' => $booking_subheading,
+        'booking_points'     => $booking_points,
+        'map_embed_url'      => $map_embed_url,
+        'align'              => !empty($block['align']) ? $block['align'] : 'full',
+        'class_name'         => !empty($block['className']) ? $block['className'] : '',
+    ]
+);
